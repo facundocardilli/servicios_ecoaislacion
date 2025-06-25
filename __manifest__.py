6b1df1f52df29a9de10395f52b6e29be24f5c568
facundocardilli <elfacu0707@gmail.com>
2025-06-25 11:37:00 -0300

    Add access rights and manifest entry

--- __manifest__.py
+++ __manifest__.py
@@ -4,8 +4,9 @@
     "version": "1.0",
     "depends": ["base", "sale", "hr", "product"],
     "data": [
+        "security/ir.model.access.csv",
         "views/obras_views.xml",
-        "views/registro_diario_views.xml"
+        "views/registro_diario_views.xml",
     ],
     "installable": True,
 }
